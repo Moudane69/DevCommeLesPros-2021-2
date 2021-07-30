144fdd4a6c6b7edd1ca8d05ae7106f0020706d8e
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-07-30 05:14:11 +0200

    completion de la fonction chiffre_ROT13, tout les test du ROT13.c Passent avec succes

--- lib/ROT13.c
+++ lib/ROT13.c
@@ -1,11 +1,62 @@
 #include "ROT13.h"
-
+#include <stdlib.h>
+#include <string.h>
 #include <stddef.h>
 
-char* chiffre_ROT13(
-    char* clair)    // Texte en clair qui sera modifié.
-{
-    return NULL;
+char* chiffre_ROT13(char* clair){    // Texte en clair qui sera modifié.
+                   
+                   
+                   
+                   
+                    int a , b ;
+                    int i;    
+                    size_t taille= strlen(clair);
+                    char* resultat = (char*) malloc(1+taille);
+                    int decalage ;
+                    
+                    
+
+
+                    for( i=0 ; i<taille ; i++){
+
+
+                        if(clair[i] <='Z' && clair[i] >='A'){
+                                            
+                                a = (clair[i] - 'A' + 13 );
+                                b = 'Z' - 'A' + 1 ;// nombre totale des alphabets;
+                                decalage = a % b ; 
+                                if (decalage < 0)
+                                {
+                                    resultat[i] ='Z'+decalage+1 ;
+                                }else{
+                                    resultat[i] ='A'+ decalage;
+                                };
+
+                                }else if(clair[i] <= 'z' && clair[i] >= 'a'){
+                        
+                                    a = (clair[i] - 'a' + 13 );
+                                    b = 'z' - 'a' + 1; // nombre totale des alphabets;
+                                    decalage = a % b ; //congruence, pour voir si il y a suffisealent de place 
+                                    if (decalage < 0)
+                                    {
+                                        resultat[i] ='z'+decalage+1 ;
+                                    }else{
+                                        resultat[i] ='a'+ decalage;
+                                    };  
+
+                                
+                                }else{
+                        
+                                        resultat[i] = clair[i];
+                                } 
+                    }
+                
+                
+                    resultat[taille] = '\0'; // caractere de la fin du chaine
+                    
+                    
+        return resultat;
+
 }
 
 char* dechiffre_ROT13(
