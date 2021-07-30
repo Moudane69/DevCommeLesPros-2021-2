af2ee565954b06e6bf439afbeb77301a9bee62c6
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-07-30 05:16:15 +0200

    complétion de la fonction dechiffre_ROT13 , les test du rot13 passent !

--- lib/ROT13.c
+++ lib/ROT13.c
@@ -59,8 +59,56 @@ char* chiffre_ROT13(char* clair){    // Texte en clair qui sera modifié.
 
 }
 
-char* dechiffre_ROT13(
-    char* chiffre)  // Texte chiffré qui sera modifié.
-{
-    return NULL;
+char* dechiffre_ROT13(char* chiffre){  // Texte chiffré qui sera modifié.
+
+                int a , b ;
+                int i;    
+                size_t taille= strlen(chiffre);
+                char* resultat = (char*) malloc(1+taille);
+                int decalage ;
+                
+            
+
+
+                for( i=0 ; i<taille ; i++){
+
+
+                    if(chiffre[i] <='Z' && chiffre[i] >='A'){
+                                        
+                            a = (chiffre[i] - 'A' -13 );
+                            b = 'Z' - 'A' + 1 ;// nombre totale des alphabets;
+                            decalage = a % b ; 
+                            if (decalage < 0)
+                            {
+                                resultat[i] ='Z'+decalage+1 ;
+                            }else{
+                                resultat[i] ='A'+ decalage;
+                            };
+
+                    }else if(chiffre[i] <= 'z' && chiffre[i] >= 'a'){
+                    
+                            a = (chiffre[i] - 'a' - 13 );
+                            b = 'z' - 'a' + 1; // nombre totale des alphabets;
+                            decalage = a % b ; //congruence, pour voir si il y a  de places suffisantes
+                            if (decalage < 0)
+                            {
+                                resultat[i] ='z'+decalage+1 ;
+                            }else{
+                                resultat[i] ='a'+ decalage;
+                            };
+
+                            
+                    }else{
+                    
+                                resultat[i] = chiffre[i];
+                    } 
+                }
+            
+            
+                resultat[taille] = '\0'; // caractere de la fin du chaine
+                    
+    
+    return resultat;
+
 }
+
