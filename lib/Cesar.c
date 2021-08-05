54568716b0f1e8ffc40bef30dbc6ebb926ac50f8
Hamza Moudane <78927840+Moudane69@users.noreply.github.com>
2021-08-06 01:33:50 +0200

    Update Cesar.c

--- lib/Cesar.c
+++ lib/Cesar.c
@@ -12,7 +12,7 @@ char* chiffre_Cesar( char* clair,char const cle)         // Texte en clair qui s
     int a , b , c ;
     size_t taille = strlen(clair);
     char * resultat =(char *)malloc(taille+1);
-    int decalage = cle-'a';
+    int decalage = cle-'a'; // dans la table ascii les lettre Majuscules viennent avant les  minusculs
     
      for( i=0 ; i<taille ; i++){
 
@@ -58,8 +58,8 @@ char cesar(char caractere, int decalage, char debut, char fin){
     //fixons le décalage du nouveau caractère depuis le début de l'alphabet
     
     int a= (caractere-debut+decalage );
-    int b=(fin-debut+1 ) ; 
-    int nouv_decalage= a%b;
+    int b=(fin-debut+1 ) ; // nombre totale des caracteres dans l'intervalle renseigné dans les paramettres d'entrées
+    int nouv_decalage= a%b; 
     //nous devrons commencer de la fin de l'alphabet  si le nouveau décalage est négatif
        
         if (nouv_decalage < 0)
@@ -78,7 +78,7 @@ char* dechiffre_Cesar(char* chiffre, char const cle)     // Texte chiffré qui s
                     int i = 0 ;                 
                     size_t taille = strlen(chiffre);
                     char * resultat =(char *)malloc(taille+1);
-                    int decalage = cle-'a';
+                    int decalage = cle-'a';// dans la table ascii les lettre Majuscules viennent avant les  minusculs
                     
                     for( i=0 ; i<taille ; i++){
 
